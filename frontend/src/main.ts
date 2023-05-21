8f21fc0b698d400db981968707c8fb0f7fb8bfce
Appifo <appifodev@gmail.com>
2023-05-21 10:56:52 +0500

    Frontend: Setup Vuex store

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,10 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 import './assets/global.css'
 import './assets/themes.css'
 
-const app = createApp(App)
+export const app = createApp(App)
 
 app.use(router)
+app.use(store)
 app.mount('#app')
