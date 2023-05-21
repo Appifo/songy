214d6c763d9d80c1de78536d4fefc3c5194080c4
Appifo <appifodev@gmail.com>
2023-05-21 11:20:25 +0500

    Frontend: Added songs interface

---
+++ frontend/src/interfaces/songs.d.ts
@@ -0,0 +1,12 @@
+export interface SongData {
+    _id: number,
+    title: string,
+    label: string,
+    thumbnail: string,
+    singer: string,
+    released: string,
+    duration: string,
+    audioBitrate: string,
+    fileName: string,
+    description?: string
+}
