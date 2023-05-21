11c4df5dd03ce1b6f80039869d5ac54ad8f8b990
Appifo <appifodev@gmail.com>
2023-05-21 11:03:48 +0500

    Frontend: Updated dynamic title for documents and added .env file

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -6,21 +6,25 @@ const routes: Array<RouteRecordRaw> = [
     path: '/',
     name: 'Dashboard',
     component: Home,
+    meta: { title: (route: { name: any }) => `Songy | ${route.name}` }
   },
   {
     path: '/browse',
     name: 'Browse',
     component: () => import(/* webpackChunkName: "browse" */ '../views/Browse.vue'),
+    meta: { title: (route: { name: any }) => `Songy | ${route.name}` }
   },
   {
     path: '/favorite',
     name: 'Favorite',
     component: () => import(/* webpackChunkName: "favorite" */ '../views/Favorite.vue'),
+    meta: { title: (route: { name: any }) => `Songy | ${route.name}` }
   },
   {
     path: '/register',
     name: 'About',
     component: () => import(/* webpackChunkName: "register" */ '../views/Register.vue'),
+    meta: { title: (route: { name: any }) => `Songy | ${route.name}` }
   }
 ]
 
@@ -29,5 +33,9 @@ const router = createRouter({
   routes,
   linkActiveClass: 'active-nav-link'
 })
+
+router.afterEach((to: any, from) => {
+  document.title = to.meta.title(to) || process.env.DEFAULT_TITLE;
+});
 
 export default router
