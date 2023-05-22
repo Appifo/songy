9b43709c4b85ceccd73dc73fa93b5f5e053c6c1a
Appifo <appifodev@gmail.com>
2023-05-22 08:56:34 +0500

    Backend: Setup public path for public and songs folder

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,12 +2,17 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const cors = require("cors");
+const path = require("path");
 const app = express();
 const songs = require("./songs-list.json");
 
 app.use(morgan("combined"));
 app.use(bodyParser.json());
 app.use(cors());
+
+app.use("/", express.static("public"));
+//  Serves all the request which includes /songs in the url from songs folder
+app.use("/songs", express.static(path.join(__dirname + "songs")));
 
 app.get("/status", (req, res) => {
   res.send({
